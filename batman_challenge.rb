3171d8b1764b4c5696d6e178d4d8b167e1282779
vicfriedman <victoria.m.friedman@gmail.com>
2015-05-06 10:25:15 -0400

    solution

--- batman_challenge.rb
+++ batman_challenge.rb
@@ -6,33 +6,43 @@ class Gotham
   end
 
   def send_bat_signal(crime)
-    # Return a string that asks for Batman's help with a specific crime!
-
+    "We need your help with a #{crime} Batman!"
   end
 
   def count_villains
-    # Determine how many villains are in the @villains array
-    # If there are less than three return the string "Batman's got this!", otherwise return a string asking Robin for help
-
+    if @villains.length < 3
+      return "Batman's got this!"
+    else 
+      "Batman needs your help Robin!"
+    end
   end
 
   def add_hero(hero)
     # Batman needs help!
-    # Write out the code to add a hero to the heroes array
-
+    # Write out the code to add a hero to the heroes arrayr
+    @heroes.push(hero)
   end
 
   def defeat_villain
-    # Batman is picking off Gotham's enemies one at a time
-    # Write a method that removes the last enemy in the array and declares that villain's defeat 
-    
+  #   # Batman is picking off Gotham's enemies one at a time
+  #   # Write a method that removes the last enemy in the array and declares that villain's defeat 
+    villian = @villains.pop
+    "Batman has defeated #{villian}!"
   end
 
   def catalogue_weapons
-    # The city is helping Batman take stock of his enemies
-    # Write out code to create a hash with key value pairs for each villain and their weapon of choice
-    # Do this by iterating over the @villains array and asking for input on what each villain's weapon should be
-    # Your last line of code should return your completed hash 
+  #   # The city is helping Batman take stock of his enemies
+  #   # Write out code to create a hash with key value pairs for each villain and their weapon of choice
+  #   # Do this by iterating over the @villains array and asking for input on what each villain's weapon should be
+  #   # Your last line of code should return your completed hash 
+  
+  hash = {}
+  @villains.each do |villain|
+    puts "What weapon does #{villain} have?"
+    weapon = gets.chomp
+    hash[villain] = weapon
+  end
+  hash
   end
 end
 
